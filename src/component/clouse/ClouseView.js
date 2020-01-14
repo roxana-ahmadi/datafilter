12634106292e8d77dd961d5e5a45c09689869035
rox <roxaaan@yahoo.com>
2020-01-14 10:24:31 +0330

    second commit

---
+++ src/component/clouse/ClouseView.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Button, Input, DatePicker, Select, Switch } from 'antd';
+
+const ClouseView = (props) => {
+    const { deleteClouse, index, oprandData } = props
+    const InputGroup = Input.Group;
+    const { Option } = Select;
+    console.log('clouseview,index ===>',index)
+    
+
+    
+    const clouseView = (
+        <div>
+            
+       <Button>add</Button>
+       <Button onClick={()=> deleteClouse(index)}>{`delete${oprandData.child[index]}`}</Button>
+        </div>
+    )
+    return (clouseView)
+}
+
+export default ClouseView
---
+++ src/component/oprand/oprandController.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Parse from 'parse'
+import {withEmitter, withState, withHandlers, withLifecycle, pipe  } from '../../util'
+
+
+const ClouseQuery = Parse.Object.extend("ClouseQuery")
+
+const addClouse = ({oprandData, setOprandData}) => () => {
+  const query = new Parse.Query(ClouseQuery)
+  oprandData.child.push(query)
+  setOprandData({...oprandData})
+}
+
+const deleteClouse = ({oprandData, setOprandData}) => (index) => {
+  console.log('on delte',oprandData.child,index)
+  oprandData.child.splice(index,1)
+  console.log('after delte',oprandData.child)
+
+  setOprandData({...oprandData})
+  console.log('after set data',oprandData.child)
+
+}
+
+const oprandController = pipe(
+    withState( ({ op:'and', child: [1,2,3,4,5,6] }), 'oprandData', 'setOprandData'),
+    withEmitter(),
+    withHandlers({
+      addClouse,
+      deleteClouse,
+    }),
+    withLifecycle({}),
+  )
+
+export default oprandController
+  
