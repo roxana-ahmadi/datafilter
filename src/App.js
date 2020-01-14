b3703bbe7b4ebb70e31045871279666811643a03
rox <roxaaan@yahoo.com>
2020-01-14 10:28:42 +0330

    second commit

--- src/App.js
+++ src/App.js
@@ -1,84 +1,113 @@
 import React from 'react';
 import Parse from 'parse'
 import Clouse from './component/Clouse'
-import DatafilterView from './component/DataFilterView'
-import Oprand , {oprandController} from './component/Oprand'
+import Oprand, { oprandController } from './component/Oprand'
 import queryGenerator from './component/queryGenerator'
+import OprandView from'./component/oprand/OprandView'
 import { Tree, Button } from 'antd';
 
 import './App.css';
 
 
 
-function App(props) {
+function App() {
 
-    const { treeData, setTreeData } = queryGenerator(props)
-    let childIndex = 0
-	
-    const queryTreeNode =  (data) => {
-		const { TreeNode } = Tree;
-		
-        console.log('query tree node start',data)
-        if (data.child) {
-            return (<TreeNode title={<Oprand op = {data.op} setTreeData = {setTreeData} treeData = {treeData} oprandNode = {data}/>}>
-                {
-                    data.child.map(x => {
-                       console.log('lll')
-                       return  queryTreeNode(x) 
-                    })
-                }
-            </TreeNode>)
-        }
-        
-        else{
-			
-        	return (<TreeNode title = {<Clouse clouseQuery = {data} setTreeData = {setTreeData} treeData = {treeData} />} />)
-        }
-    }
+    // const { treeData, setTreeData } = queryGenerator()
+    // const { deleteClouse, oprandData } = oprandController()
     
+    // const queryTreeNode = (data, index) => {
+    //     const { TreeNode } = Tree;
+    //     let childIndexx = index
+    //     let opnode 
+    //     //console.log('query tree node start', data)
+  
+
+
+    //     if (data.child) {
+    //         opnode = data
+    //         console.log('opnode from app+++++++++', opnode)
+    //         return (<TreeNode title={<Oprand op={data.op} setTreeData={setTreeData} treeData={treeData} oprandNode={data} childIndex={childIndexx} />}>
+    //             {
+                    
+    //                 data.child.map((item, index) => {
+    //                     return queryTreeNode(item, index)
+    //                 })
+    //             }
+    //         </TreeNode>)
+    //     }
+
+    //     else {
+            
+    //         return (<TreeNode title={<Clouse clouseQuery={data} setTreeData={setTreeData} treeData={treeData} clouseIndex={childIndexx} deleteClouse = {deleteClouse} />} />)
+    //     }
+    // }
+
+
+
+    // const treeView = (
+    //     <Tree>
+    //         {queryTreeNode(treeData,0)}
+    //     </Tree>
+
+    // )
     
-	
-    const treeView = (
-        <Tree>
-            {queryTreeNode(treeData)}
-        </Tree>
-		
-    )
 
-    // const makeMainQuery =  (treeData) => {
-    //     const ClouseQuery = Parse.Object.extend("ClouseQuery")
-    //     const query = new Parse.Query(ClouseQuery)
+    // const makeMainQuery = (treeData) => {
+    //     // let q1 = new Parse.Query(ClouseQuery)
+    //     // let q2 = new Parse.Query(ClouseQuery)
+    //     // let q = new Parse.Query(ClouseQuery)
+    //     // treeData.mainQuery = treeData.mainQuery._andQuery([q1,q2])
+    //     // console.log(treeData.mainQuery)
 
-    //     //var mainQuery =  Parse.Query.and(treeData.child[0].,treeData.child[1].query)
+    //     //  treeData.mainQuery = Parse.Query.and(
+    //     //     treeData.mainQuery,
+    //     //     Parse.Query.or(new Parse.Query(ClouseQuery), new Parse.Query(ClouseQuery))
+    //     //   );
 
+    //     console.log('majidmajid', treeData)
     //     if (treeData.child) {
     //         let op = treeData.op
-           
+
     //         treeData.child.map(x => {
-    //             if(op == 'and')
-    //                 return treeData.mainQuery = Parse.Query.and(treeData.mainQuery,makeMainQuery(x))
-    //             if(op == 'or'){
-    //             console.log('op',treeData.op)
-    //                 return treeData.mainQuery = Parse.Query.or(treeData.mainQuery,makeMainQuery(x))}
+
+    //             // console.log('tredata mian query==>',treeData.mainQuery)
+    //             // console.log('x==>',x)
+    //             if (!x.op) {
+    //                 if (op == 'and') {
+    //                     console.log('op', op)
+    //                     treeData.mainQuery = Parse.Query.and(treeData.mainQuery, x)
+    //                 }
+    //                 else {
+    //                     treeData.mainQuery = Parse.Query.or(treeData.mainQuery, x)
+    //                 }
+    //             }
+    //             else {
+    //                 return makeMainQuery(x)
+    //             }
+
+    //             // else{
+    //             // console.log('op',treeData.op)
+    //             //     return treeData.mainQuery = Parse.Query.or(treeData.mainQuery,makeMainQuery(x))
+    //             // }
     //         })
-    
+
     //     }
-    //     else{
-            
+    //     else {
+    //         // console.log('tt',treeData)
     //         return treeData
     //     }
     // }
-        
-    return(
-    	<div>
-	     {treeView}
-		 {console.log('treedata ===>',treeData)}
-         {/* <Button onClick = { () => makeMainQuery(treeData) }>search</Button>
-         <Button onClick = {() => console.log('treedata mainquery =>',treeData.mainQuery)}>showtd</Button> */}
- 	   </div>
-   
-   
-  )
+
+    return (
+        <div>
+            <OprandView />
+            {/* {treeView}
+            <Button onClick={() => makeMainQuery(treeData)}>search</Button>
+            <Button onClick={() => console.log('treedata mainquery =>', treeData.mainQuery)}>showtd</Button> */}
+        </div>
+
+
+    )
 }
 
 
